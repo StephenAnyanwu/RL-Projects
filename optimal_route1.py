bd32f4f3efaefd10cdfc9c2271d5fd5d9c25f90f
StephenAnyanwu <98687335+StephenAnyanwu@users.noreply.github.com>
2022-02-21 20:45:01 +0100

    Update optimal_route1.py
    In this RL project, the agent randomly chooses a targeted location while we choose a starting location in an input prompt for the agent and the 
    agent returns an optimal route to the targeted location in a list. optimal route is return if the locations (both starting and targeted) are 
    situated in the environment. 
    See  optimal_route_image1.png in Image repository  file that contains the diagram description of the environment.

--- optimal_route1.py
+++ optimal_route1.py
@@ -2,10 +2,10 @@ import random
 import numpy as np
 
 '''
-In this RL porject, the agent randomly chooses a targeted location while we choose a starting location in an input prompt for the agent and the 
+In this RL project, the agent randomly chooses a targeted location while we choose a starting location in an input prompt for the agent and the 
 agent returns an optimal route to the targeted location in a list. optimal route is return if the locations (both starting and targeted) are 
 situated in the environment. 
-See " optimal_route_image1.png in Image repository " file that contains the diagram description of the environment. 
+See  optimal_route_image1.png in Image repository  file that contains the diagram description of the environment. 
 '''
 
 gamma = 0.75 # discount factor
